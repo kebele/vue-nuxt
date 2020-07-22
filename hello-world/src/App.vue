804e42cb8f09a41f3ce9db3ef00e0edfccdeb307
kebele <ykemalbeler@gmail.com>
2020-07-23 02:49:59 +0300

    ekleme

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,12 +1,26 @@
 <template>
-  <div></div>
+  <div>
+    <post content="real madrid"></post>
+    <post :content="content"></post>
+  </div>
 </template>
 
 <script>
+import Post from './components/Post'
 export default {
   data() {
-    return {};
+    return {
+      content:[
+        {number : 1, content:'one'},
+        {number : 2, content:'two'},
+        {number : 3, content:'three'},
+        {number : 4, content:'four'},
+      ]
+    };
   },
+  components : {
+    Post,
+  }
 };
 </script>
 
---
+++ hello-world/src/components/Post.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <!-- <h3>{{ content }}</h3>     -->
+        <h6 v-for="(i,index) in content" :key="index">{{i.number}} {{ i.content }}</h6>
+    </div>
+</template>
+<script>
+export default {
+    props : {
+        // content : String
+        content : Array
+    }
+}
+</script>
