de9500472a0aad8999afdea27a94c6bacc50fb4d
kebele <ykemalbeler@gmail.com>
2020-07-25 09:51:15 +0300

    ekleme

---
+++ hello-world/src/components/Big.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <span>this is big</span>
+        <label for="">
+            <input type="text" v-model="title" @keyup="eventBusFunc">
+        </label>
+    </div>
+</template>
+<script>
+import { eventBus } from '../main'
+export default {
+    data(){
+        return {
+            title : '',
+        }
+    },
+    methods: {
+        eventBusFunc(){
+            eventBus.$emit("fromBig", this.title)
+        }
+    },
+}
+</script>
---
+++ hello-world/src/components/Small.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <span>this is small</span>
+    <br />
+    <span>{{ message }}</span>
+  </div>
+</template>
+<script>
+import { eventBus } from "../main";
+
+export default {
+  data() {
+    return {
+      message: ""
+    };
+  },
+  created() {
+    eventBus.$on("fromBig", title => {
+      this.message = title;
+    });
+  }
+};
+</script>
