3ad620fc418fbb63266b642e4f05d416e7f3ad11
kebele <ykemalbeler@gmail.com>
2020-07-26 23:11:15 +0300

    ekleme

--- 3_local_sto_session_sto_cookies/storages/src/App.vue
+++ 3_local_sto_session_sto_cookies/storages/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    
+    <input type="text" v-model="userName">
+    <button @click="setCookies">set cookie</button>
+    <br>
+    <button @click="removeCookies">remove cookies</button>
   </div>
 </template>
 
@@ -9,11 +12,22 @@
 export default {
   data(){
     return {
-    
+    userName : '',
     }
   },
   methods: {
-  
+    setCookies(){
+      //npm sayfasındaki set satırını alıp düzenleyeceğiz, aşağıya npm den aldığım şablonu koydum
+      // this.$cookies.set("user_session","25j_7Sl6xDq2Kc3ym0fmrSSk2xV2XkUkX")
+      //30MIN cookie nin geçerli olacağı zaman (expire time), yine npm sayfasında var, eğer süresiz kalmasını istiyorsak süre kısmına Infinity koyarız, 
+      this.$cookies.set("token",this.userName,"30MIN")
+    },
+    removeCookies(){
+      //token ı silmek için ise yine npm sayfasında, 
+      //this.$cookies.remove("token");
+      this.$cookies.remove("token");
+      //hemen cookies e yansımayabilir yeniden cokies e geresek gittiğini görürüz
+    }
   }  
 }
 </script>
