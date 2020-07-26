e531d3f4a3de7ef54d3df911ceac33a3f05ee838
kebele <ykemalbeler@gmail.com>
2020-07-26 22:45:49 +0300

    ekleme

--- 3_local_sto_session_sto_cookies/storages/src/App.vue
+++ 3_local_sto_session_sto_cookies/storages/src/App.vue
@@ -1,48 +1,20 @@
 <template>
   <div id="app">
-    <label for="">
-      <input type="text" v-model="value">
-    </label>
-    <br>
-    <button @click="saveValue">save</button>
-    <hr>
-    <span>{{ newValue }}</span>
-    <br>
-    <button @click="removeLocalData">sil</button>
+    
   </div>
 </template>
 
 <script>
 
-
 export default {
   data(){
     return {
-      value : '',
-      newValue : '',
-      foo : { name : 'vue', age : 34},
+    
     }
   },
   methods: {
-    saveValue(){
-      //stringfy kullanacağımız için aşağıyı yorum yaptım
-      // localStorage.setItem('input-value', this.value);
-      // this.value = ''
-      localStorage.setItem('input-value', JSON.stringify(this.foo));
-
-    },
-    removeLocalData(){
-      localStorage.removeItem('input-value');
-      // localStorage.clear() //her şeyi siler
-    }
-  },
-  mounted() {
-    if(localStorage.getItem('input-value')){
-      // this.newValue = localStorage.getItem('input-value')
-      //stringify ile kaydettiğimiz object i getirmek içn
-      this.newValue = JSON.parse(localStorage.getItem('input-value'))
-    }
-  },
+  
+  }  
 }
 </script>
 
---
+++ 3_local_sto_session_sto_cookies/storages/src/App2.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="value">
+    <br>
+    <button @click="saveData">save</button>
+  </div>
+</template>
+
+<script>
+var CryptoJS = require("crypto-js");
+export default {
+  data(){
+    return {
+      value : '',
+    }
+  },
+  methods: {
+   saveData(){
+    //  var ciphertext = CryptoJS.AES.encrypt('my message', 'secret key 123').toString();
+    //yukarıdaki şablon olarak npm sayfasından aldığımız kullanım
+    //  var ciphertext = CryptoJS.AES.encrypt(this.value, 'secret key 123').toString();
+      const foo = CryptoJS.AES.encrypt(this.value, 'secret key 123').toString()
+      localStorage.setItem('userData', foo);
+      console.log(foo);
+      //şifrelenmiş halde aldık, şimdi bunu decode edelim
+      var bytes  = CryptoJS.AES.decrypt(foo, 'secret key 123');
+      var originalText = bytes.toString(CryptoJS.enc.Utf8);
+      console.log(originalText);
+
+
+   }
+  }  
+}
+</script>
+
+<style>
+
+</style>
