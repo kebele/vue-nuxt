c6de9e90021cc617947a58a4b5e6d211009c004a
kebele <ykemalbeler@gmail.com>
2020-07-27 22:18:08 +0300

    ekleme

--- 3_local_sto_session_sto_cookies/storages/src/App.vue
+++ 3_local_sto_session_sto_cookies/storages/src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
     <input type="text" v-model="userName" />
+    <br>
     <button @click="setCookies">set cookie</button>
     <br />
     <button @click="removeCookies">remove cookies</button>
     <br />
-    <button @click="bcryptToken(userName)">bcrypt</button>
+    <!-- parametre göndermektense parametre meselsini methodun içinde kapsam ile çözelim -->
+    <!-- <button @click="bcryptToken(userName)">bcrypt</button> -->
+    <button @click="bcryptToken">bcrypt</button>
+    <br />
+    <!-- <button @click="compareBcrypt(userName, hashValue)">bcrypt karşılaştır</button> -->
+    <button @click="compareBcrypt">bcrypt karşılaştır</button>
   </div>
 </template>
 
@@ -16,6 +22,7 @@ export default {
   data() {
     return {
       userName: "",
+      hashValue: ""
     };
   },
   methods: {
@@ -31,26 +38,47 @@ export default {
       this.$cookies.remove("token");
       //hemen cookies e yansımayabilir yeniden cokies e geresek gittiğini görürüz
     },
-    bcryptToken(userName) {
+    // bcryptToken(userName) {
+    bcryptToken() {
       // aşağısı şablon olarak npm sayfasından aldığımız
       // bcrypt.genSalt(10, function(err, salt) {
       //   bcrypt.hash("B4c0//", salt, function(err, hash) {
       //     // Store hash in your password DB.
       //   });
       // });
+      const dataValue = this; //temolate den parametre yollamak yerine...
       bcrypt.genSalt(10, function(err, salt) {
-        bcrypt.hash(userName, salt, function(err, hash) {
+        bcrypt.hash(dataValue.userName, salt, function(err, hash) {
           if (err) {
             console.log(err);
             return;
-          } else {
-            console.log(`HASH : ${hash}`);
-            //problemsiz şifrelenmiş hali geldi, şimdi check password durumunun nasıl yapıldığına bakalım, yine npm sayfasına gideceğiz
           }
+          // else {
+          // this.hash = hash;
+          console.log(`HASH : ${hash}`);
+          //problemsiz şifrelenmiş hali geldi, şimdi check password durumunun nasıl yapıldığına bakalım, yine npm sayfasına gideceğiz,
+          //burada parolaları artık doğrudan kaydetmektense hash şeklini kaydederiz, çok daha güvenli olur, db lerede bu türden kritik bilgileri keydederken hash leme kullanılır, burada gelen hash ı da bir değişkene atarsak karşılaştırma yaparken kolaylıık olur, check password konusuna bakalım, parolayı hash şekklini kaydettik peki birisi gelip parolayı girdiğinden bunu nasıl kontrol edeceğiz?, bunun için compare() var.
+          // }
+          dataValue.hashValue = hash
         });
       });
+      // this.hashValue = this.hash
     },
-  },
+    // compareBcrypt(userName, hash) {
+    compareBcrypt() {
+      //şablonu yapıştıralım
+      // bcrypt.compare("B4c0//", hash, function(err, res) {
+      // res === true
+      // });
+      const dataValue = this; //temolate den parametre yollamak yerine...
+      bcrypt.compare(dataValue.userName, dataValue.hashValue, function(err, res) {
+        // res === true
+        if (res) {
+          console.log("karşılaştırma başarılı");
+        }
+      });
+    }
+  }
 };
 </script>
 
