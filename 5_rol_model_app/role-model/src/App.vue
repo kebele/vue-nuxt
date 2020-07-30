2a37a80b8c577973a4b913aeb045e23da9e29b9e
kebele <ykemalbeler@gmail.com>
2020-07-30 14:53:53 +0300

    ekleme

---
+++ 5_rol_model_app/role-model/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="4">
+          <AddRoleModel></AddRoleModel>
+        </b-col>
+        <b-col sm="8">
+
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import AddRoleModel from './components/AddRoleModel'
+
+export default {
+  components: {
+    AddRoleModel,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 5_rol_model_app/role-model/src/components/AddRoleModel.vue
@@ -0,0 +1,18 @@
+<template>
+    <b-form>
+        <b-form-input class="mt-2" v-model="text" placeholder="What is your role model name?"></b-form-input>
+        <b-form-input class="mt-2" v-model="text" placeholder="What is his job?"></b-form-input>
+        <b-form-input class="mt-2" v-model="text" placeholder="What is his nationality?"></b-form-input>
+        <b-form-datepicker v-model="value" class="mt-2 mb-2"></b-form-datepicker>
+    </b-form>
+
+</template>
+<script>
+export default {
+    data(){
+        return {
+            text : ''
+        }
+    }
+}
+</script>
