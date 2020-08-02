08a9298a37f2673a692c95f0ac833cf700dcf3ad
kebele <ykemalbeler@gmail.com>
2020-08-02 17:40:22 +0300

    ekleme

--- 7_vue_resource/node_modules/vue-resource/src/App.vue
+++ 7_vue_resource/node_modules/vue-resource/src/App.vue
@@ -15,7 +15,21 @@ export default {
   },
   components: {
     
-  }
+  },
+  mounted() {
+    {
+  // GET /someUrl
+  this.$http.get('https://reqres.in/api/users').then(response => {
+ 
+    // get body data
+    // this.someData = response.body;
+ console.log(response)
+  }, response => {
+    // error callback
+    console.log(response)
+  });
+}
+  },
 }
 </script>
 
