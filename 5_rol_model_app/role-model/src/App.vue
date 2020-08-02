35b6b916751afa907dd50de6353d2cc5f4e5b619
kebele <ykemalbeler@gmail.com>
2020-08-02 08:19:53 +0300

    ekleme

--- 5_rol_model_app/role-model/src/App.vue
+++ 5_rol_model_app/role-model/src/App.vue
@@ -1,47 +1,48 @@
 <template>
   <div>
-    <b-container class="bv-example-row">
-      <b-row>
-        <b-col sm="4" class="mb-5">
-          <AddRoleModel @fromAddRoleModel="updateCard($event)" />
-        </b-col>
-        <b-col sm="8" class="cardContainer">
-          <ModelCard
-            v-for="(item, index) in card"
-            :key="index"
-            :index="index"
-            :modelName="item[0]"
-            :modelJob="item[1]"
-            :modelNationality="item[2]"
-            :modelBirthday="item[3]"
-            :modelAbout="item[4]"
-            :modelPic="item[5]"
-            :tags="item[6]"
-            @updateEmitIndex="activeIndex($event)"
-            @deleteModelOpen="deleteModelOpen($event)"
-          />
-          <UpdateModelCard
-            :modelName="card[activeNumber][0]"
-            :modelJob="card[activeNumber][1]"
-            :modelNationality="card[activeNumber][2]"
-            :modelBirthday="card[activeNumber][3]"
-            :modelAbout="card[activeNumber][4]"
-            :modelPic="card[activeNumber][5]"
-            :tags="card[activeNumber][6]"
-            v-if="showModel"
-            @closeModel="updateOkey($event)"
-            @hideUpdateModel="hideUpdateModel()"
-          />
-          <DeleteModelCard
-            v-if="showModelDelete"
-            :index="activeNumber"
-            :modelName="card[activeNumber][0]"
-            @deleteModel="deleteModel()"
-            @deleteModelHide="deleteModelHide()"
-          />
-        </b-col>
-      </b-row>
-    </b-container>
+    <b-row>
+      <b-col sm="4" class="ml-2">
+        <AddRoleModel @fromAddRoleModel="updateCard($event)" />
+      </b-col>
+      <transition-group sm="12" class="role-model" name="role-model" tag="b-col">
+        <ModelCard
+          v-for="(item, index) in card"
+          :key="index"
+          :index="index"
+          :modelName="item[0]"
+          :modelJob="item[1]"
+          :modelNationality="item[2]"
+          :modelBirthday="item[3]"
+          :modelAbout="item[4]"
+          :modelPic="item[5]"
+          :tags="item[6]"
+          @updateEmitIndex="activeIndex($event)"
+          @deleteModelOpen="deleteModelOpen($event)"
+          
+        />
+        <UpdateModelCard
+          :modelName="card[activeNumber][0]"
+          :modelJob="card[activeNumber][1]"
+          :modelNationality="card[activeNumber][2]"
+          :modelBirthday="card[activeNumber][3]"
+          :modelAbout="card[activeNumber][4]"
+          :modelPic="card[activeNumber][5]"
+          :tags="card[activeNumber][6]"
+          v-if="showModel"
+          @closeModel="updateOkey($event)"
+          @hideUpdateModel="hideUpdateModel()"
+          :key="randomKey"
+        />
+        <DeleteModelCard
+          v-if="showModelDelete"
+          :index="activeNumber"
+          :modelName="card[activeNumber][0]"
+          @deleteModel="deleteModel()"
+          @deleteModelHide="deleteModelHide()"
+          :key="randomKey"
+        />
+      </transition-group>
+    </b-row>
   </div>
 </template>
 
@@ -58,6 +59,7 @@ export default {
       showModel: false,
       showModelDelete: false,
       activeNumber: -1,
+      randomKey : Math.random()
     };
   },
   methods: {
@@ -86,23 +88,43 @@ export default {
       this.activeNumber = -1;
       console.log(this.card);
     },
-    deleteModelHide(){
+    deleteModelHide() {
       this.showModelDelete = false;
     },
-    hideUpdateModel(){
-      this.showModel = false
-    }
-
+    hideUpdateModel() {
+      this.showModel = false;
+    },
   },
 };
 </script>
 
 <style>
-.cardContainer {
+.role-model{
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: space-around;
-  margin-top: 20px;
+  margin-top: 40px;
+  backface-visibility: hidden;
+  transform-origin: 10% 50%;
+  z-index: 1;
+}
+
+.role-model-move{
+  transition : all 600ms ease-out
+  /* ease-out yavaş başla hızlı bitir */
+}
+
+.role-model-enter-active{
+  transition: all 300ms ease-out;
+}
+.role-model-leave-active{
+  transition: all 300ms ease-out;
+  position: absolute;
+  z-index: 1;
+}
+.role-model-enter,
+.role-model-leave-to{
+  opacity : 0
 }
 </style>
