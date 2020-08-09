9f334a5c62ac3e4f4e8c2d38f7e075bc8860a815
kebele <ykemalbeler@gmail.com>
2020-08-09 22:16:19 +0300

    ekleme

--- 11_sepet_app/sepet/src/components/Products.vue
+++ 11_sepet_app/sepet/src/components/Products.vue
@@ -3,17 +3,20 @@
         <h2>products</h2>
         <div class="notification" v-for="(info,key) in infos" :key="key">
             <table class="{ products : products.length !== 0}">
+                <!-- eğer products da bir şeyler varsa o zaman class alacak -->
                 <tr>
                     <th>product name</th>
                     <th>price</th>
                     <th>buy</th>
                 </tr>
                 <tr v-for="product in products" :key="product.id">
+                    <!-- computed ile sate deki products ı almış olduk??? -->
                     <td>{{ product.name }}</td>
                     <td>{{ product.price }}</td>
                     <td>
                         <label>
                             <input type="number" :id="product.id" value="0">
+                            <!-- id yi dinamik verdik, inputta belli bir ürünü almamız gerekiyor seçmek için bunun içinde unique bir id ye ihtiyacımız var -->
                         </label>
                     </td>
                     <td>
@@ -26,31 +29,42 @@
 </template>
 <script>
 import { mapState } from 'vuex'
+//map ile başlayan kullanımlarda mapState ise mesela state deki ne varsa alıp kullanmamıza izin veriyor, aktif olarak yoğunlukla kullanılır
 export default {
     data(){
         return {
             infos : [],
+            // 
         }
     },
     computed: {
         ...mapState['products']
+        //mesela burada bunu this.$store.state.products şeklinde de yazabiliridik, bu şekilde de aynı işi görür daha az karakter olmuş olur
     },
     methods: {
         addToCart(product){
             if(document.getElementById(product.id).value > 1){
+                // seçilen değereimn 0 dan büyük old. kontrolu
                 const count = document.getElementById(product.id).value
                 product = {...product, count}
+                //product daki tüm elermanlar + count demek
                 this.$store.commit('addToCart', product)
+                //commit mutations ı hatırlatır, mutations dki addToCart a parametre olarak produt gönder
                 const info = product.name + 'added to cart'
+                //bildirim oluşturuyor ve infos []'a pushluyoruz
                 this.infos.push(info)
+                //3000ms sonra infos a eklediğimizi sileceğiz
                 setTimeout(() => {
                     document.getElementById(product.id).value = 0
                     this.infos.splice(this.infos.indexOf(info), 1)
+                    // index değerinden itibaren silinecek eleman ... splice kullanımı
                 }, 3000);
             } else {
+                // eğer girilen değer 0 ise, else durumu yani
                 const info = 'please enter a valid input'
                 // const info = 'lütfen uygun bir değer girin'
                 this.infos.push(info);
+                //3000ms sonra infos a eklediğimizi sileceğiz
                 setTimeout(() => {
                     document.getElementById(product.id).value = 0
                     this.infos.splice(this.infos.indexOf(info), 1)
