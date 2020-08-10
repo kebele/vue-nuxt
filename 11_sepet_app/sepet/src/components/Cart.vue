cfa0d96b4bb8ccda0705d78bfe54f4f4f14f1f39
kebele <ykemalbeler@gmail.com>
2020-08-10 04:33:15 +0300

    ekleme

---
+++ 11_sepet_app/sepet/src/components/Cart.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <h2>Cart</h2>    
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    ul{
+        list-style: none;
+    }
+    li{
+        display: flex;
+        max-width: 700px;
+        margin: auto;
+        margin-top: 10px;
+    }
+    .item-name{
+        text-align: left;
+        font-size: 18px;
+        flex: 2;
+    }
+    .item-count{
+        font-size: 25px;
+        margin-left: 10px;
+        flex: 2;
+    }
+    .item-amount{
+        text-align: left;
+        flex : 1
+    }
+    .total-amount{
+        border-top : 2px solid black;
+        border-bottom: 2px solid black;
+        padding: 10px 20% 10px 10px;
+        margin: 10px;
+        font-size: 29px;
+        text-align: right;
+    }
+    button{
+        border: none;
+        cursor: pointer;
+        padding : 5px 10px;
+    }
+</style>
--- 11_sepet_app/sepet/src/store/index.js
+++ 11_sepet_app/sepet/src/store/index.js
@@ -6,7 +6,22 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        card : [],
+        cart : [],
         products : productList,
+    },
+    mutations : {
+        addToCart({cart}, product){
+            //state deki sadece cart ı alıyoruz demek
+            const itemIndex = cart.findIndex(item => item.id === product.id)
+            //product ın id si hazırdaki cart [] da var mı?, yni sepette daha önceden aynı üründen eklenmiş mi
+            if(itemIndex === -1){
+                //sepette daha önceden aynı üründen eklenmediyse product u sepete ekle
+                cart.push(product)
+            } else {
+                //daha önceden sepette varsa miktarını arttıracağız, sepetteki ürünün id si ile ürüne erişip onun count unu; count seçilen ürünün id sinin value siydi, önceki product ın countunu alıp üstüne yeni count ekleyeceğiz,
+                // parseInt gelen değeri integer a çevirir
+                cart[itemIndex].count = cart[itemIndex].count + parseInt(product.count)
+            }
+        }
     }
 })
