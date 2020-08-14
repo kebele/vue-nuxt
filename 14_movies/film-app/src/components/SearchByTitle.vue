1439041a68233f085640771a7ede15f14ea1115e
kebele <ykemalbeler@gmail.com>
2020-08-14 14:16:27 +0300

    ekleme

--- 14_movies/film-app/src/components/SearchByTitle.vue
+++ 14_movies/film-app/src/components/SearchByTitle.vue
@@ -1,21 +1,27 @@
 <template>
-  <div>
+  <div class="searchNyTitle">
+    <div>
       <input type="text" placeholder="search by title" v-model="searchMovie" />
       <button @click="xxx">search movie by title</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name : 'SearchByTitle',
-    data(){
-        return {
-            searchMovie  : ''
-        }
-    }
-}
+  name: "SearchByTitle",
+  data() {
+    return {
+      searchMovie: "",
+    };
+  },
+};
 </script>
 
 <style>
-
+.searchByTitle {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 </style>
