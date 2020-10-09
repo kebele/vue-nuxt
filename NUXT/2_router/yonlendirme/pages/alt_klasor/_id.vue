30f8b8d30426ea9009ffa095870dfe03c0aa0ad0
kebele <ykemalbeler@gmail.com>
2020-10-10 01:19:54 +0300

    ekleme

--- NUXT/2_router/yonlendirme/pages/alt_klasor/_id.vue
+++ NUXT/2_router/yonlendirme/pages/alt_klasor/_id.vue
@@ -10,8 +10,10 @@ export default {
   methods: {
     showParams(){
       console.log(this.$route.params.id)
+      // route içindeki parametreye ulaşmak için
     }
   },
+  // validate bir vue/nuxt özelliği
   validate({params}){
     return /^\d+$/.test(params.id)
   }
