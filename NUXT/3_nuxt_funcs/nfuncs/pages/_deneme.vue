0574ab8cbc537d764c51a001015401d6360a66f6
kebele <ykemalbeler@gmail.com>
2020-10-10 08:42:18 +0300

    ekleme

---
+++ NUXT/3_nuxt_funcs/nfuncs/pages/_deneme.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    deneme
+    <br>
+    <hr>
+    {{ value }}
+  </div>
+</template>
+
+<script>
+export default {
+  // asyncData({isDev, route, store, env, params, query, req, res, redirect, error}) {
+  // bunların hepsi context olarak kullanılır
+//   asyncData (context) {
+//     console.log(context)
+//   }
+// fake api kullanarak bir örnek yapalım, axios kullanacağız, get işlemi yapacağız ve API deki posts u çekip dataaki value ye atayıp instance da yazdıracağız, bu örnegin aynısı nux dokumantasyonunda da var, async await şeklinde de yapabiliriz, sadece axios, mesela error de yakalayabbiliriz, catch ile ve kendi error sayfamıza yolayabiliriz
+// sonuç başarılı
+// önemli bir not olarak asyncData içinde kullanılan methodlar componentin içinbde başka bir yerdede kullanılıyorsa bu ikisi birleştirilir,
+  data () {
+    return {
+      value: null
+    }
+  },
+  /* async asyncData ({ params, $axios, error }) {
+    return await $axios.get(`https://jsonplaceholder.typicode.com/${params.deneme}`)
+      .then((res) => {
+        // return { value: res.data }
+        return { value: res.data[5].title } // beşinci indisteki elemanının title al
+      })
+      .catch((e) => {
+        error({ statusCode: 404, message: 'page was not found' })
+      })
+  } */
+  // bir tane daha örnek yapalım context ile ilgili, console dan baktığımızda neler var diye axioslar var, lif cycle la ilgili mount vs var, bir süreü şey var
+
+  asyncData (context) {
+    return console.log(context)
+  }
+}
+</script>
+
+<style scoped></style>
