b92cecf988dcaa5e44c27a32375db9b31188b146
kebele <ykemalbeler@gmail.com>
2020-10-12 09:14:37 +0300

    ekleme

---
+++ NUXT/3_nuxt_funcs/nfuncs_module/modules/simple.js
@@ -0,0 +1,9 @@
+export default function SimpleModule(moduleOptions){
+    // this.options ile nuxt options lara ulaşabiliriz, moduller arasında option ları paylaşmak için kullanabiliriz
+    // this.nuxt ile de nuxt ın yaşam döngüsü option larına ulaşabiliriz, 
+    this.nuxt.hook('modules:done',(modulesContainer)=>{
+        
+    })
+}
+
+modules.export.meta=require('./package.json')
--- NUXT/3_nuxt_funcs/nfuncs_module/nuxt.config.js
+++ NUXT/3_nuxt_funcs/nfuncs_module/nuxt.config.js
@@ -35,6 +35,7 @@ export default {
     '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
+    '~/modules/simple',
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
