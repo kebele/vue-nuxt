86767337a391023f86c5b9599d4e91e3cacfb0be
kebele <ykemalbeler@gmail.com>
2020-10-13 17:24:34 +0300

    ekleme

---
+++ NUXT/4_authentication/authX/pages/signup.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <!-- bootstrap vue yu projeyi kurarken eklediğimiz için gidip direkt componentlerden alıp burada kullanabiliriz -->
+    <!-- fomlardan alalım -->
+    <b-form @submit.stop.prevent>
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
+      <label for="text-password">Password</label>
+      <b-input
+        type="password"
+        id="text-password"
+        aria-describedby="password-help-block"
+      ></b-input>
+      <b-form-text id="password-help-block">
+        Your password must be 8-20 characters long, contain letters and numbers,
+        and must not contain spaces, special characters, or emoji.
+      </b-form-text>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
--- NUXT/4_authentication/authX/services/firebase.js
+++ NUXT/4_authentication/authX/services/firebase.js
@@ -3,7 +3,7 @@
 import firebase from "firebase/app"
 import "firebase/auth"
 
-//firebase ayarlarımızı tutacağımız config nesnesini hazırlayalım, firebase bize bazı değerler veriyor apikey, domain vb, bunları firebase sitesinden alalım, genel ayarlardan web uygulaması ekleyeceğiz, uygulamamıza bir isim verip kaydedelim,bize verdiği kod içinde firebaseconfig in karşısındaki nesneyi tamamen alalım ve bizim config e atayalım, 
+//firebase ayarlarımızı tutacağımız config nesnesini hazırlayalım, firebase bize bazı değerler veriyor apikey, domain vb, bunları firebase sitesinden alalım, genel ayarlardan web uygulaması ekleyeceğiz, uygulamamıza bir isim verip kaydedelim,bize verdiği kod içinde firebaseconfig in karşısındaki nesneyi tamamen alalım ve bizim config e atayalım, bunları daha sonra .env dosyasına alıp göz önünden kaldıracağız, 
 const config = {
     apiKey: "AIzaSyBCmimQ0nMdTXMBdQmcyYBtEf3WxWkU8R4",
     authDomain: "nuxt-auth-8a830.firebaseapp.com",
@@ -13,3 +13,12 @@ const config = {
     messagingSenderId: "984430755159",
     appId: "1:984430755159:web:666a6f0a047f6c6ad89212"
   }
+
+  // eğer firebase.apps yoksa app i başlat ve içine bu config i yolla eğer varsa bir şey yapma
+  !firebase.apps.length ? firebase.initializeApp(config) : ""
+
+  //şimdi bu dosyayı dışarıya, firebase.auth olarak açalım, 
+export const auth = firebase.auth()
+
+//artık sıra pages içine projedeki kullanacağımız sayfaları yapmada
+
