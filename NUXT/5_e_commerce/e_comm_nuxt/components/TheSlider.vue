6689f529a734b370e1d05578cc0e1cc47b5ff403
kebele <ykemalbeler@gmail.com>
2020-10-17 21:36:06 +0300

    update

--- NUXT/5_e_commerce/e_comm_nuxt/components/TheSlider.vue
+++ NUXT/5_e_commerce/e_comm_nuxt/components/TheSlider.vue
@@ -1,55 +1,72 @@
 <template>
-  <div>
-    <b-carousel
-      id="carousel-1"
-      v-model="slide"
-      :interval="4000"
-      controls
-      indicators
-      background="#ababab"
-      img-width="1024"
-      img-height="480"
-      style="text-shadow: 1px 1px 2px #333;"
-      @sliding-start="onSlideStart"
-      @sliding-end="onSlideEnd"
-    >
-     
+  <div class="sliderContainer">
+    <div class="sliderContent">
+      <!-- bu div i bir div içine daha alacağız, sebebi dış div sayfada 100% yer kampasım içteki slider ise biraz daha içten olsun sınırları -->
+      <b-carousel
+        id="carousel-1"
+        v-model="slide"
+        :interval="4000"
+        controls
+        indicators
+        background="#ababab"
+        img-width="1024"
+        img-height="480"
+        style="text-shadow: 1px 1px 2px #333;"
+        @sliding-start="onSlideStart"
+        @sliding-end="onSlideEnd"
+      >
+        <!-- Slides with image only -->
+        <b-carousel-slide
+          img-src="https://picsum.photos/1024/480/?image=58"
+        ></b-carousel-slide>
+        <b-carousel-slide
+          img-src="https://picsum.photos/1024/480/?image=59"
+        ></b-carousel-slide>
+        <b-carousel-slide
+          img-src="https://picsum.photos/1024/480/?image=55"
+        ></b-carousel-slide>
+        <b-carousel-slide
+          img-src="https://picsum.photos/1024/480/?image=56"
+        ></b-carousel-slide>
+      </b-carousel>
 
-      <!-- Slides with image only -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=59"></b-carousel-slide>
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=55"></b-carousel-slide>
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=56"></b-carousel-slide>
-
-      
-    </b-carousel>
-
-    <p class="mt-4">
-      Slide #: {{ slide }}<br>
-      Sliding: {{ sliding }}
-    </p>
+      <p class="mt-4">
+        Slide #: {{ slide }}<br />
+        Sliding: {{ sliding }}
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        slide: 0,
-        sliding: null
-      }
+export default {
+  data() {
+    return {
+      slide: 0,
+      sliding: null
+    };
+  },
+  methods: {
+    onSlideStart(slide) {
+      this.sliding = true;
     },
-    methods: {
-      onSlideStart(slide) {
-        this.sliding = true
-      },
-      onSlideEnd(slide) {
-        this.sliding = false
-      }
+    onSlideEnd(slide) {
+      this.sliding = false;
     }
   }
+};
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+    .sliderContainer{
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        align-items: center;
+    }
+    .sliderContent{
+        width: 80%;
+        padding-left: 20px;
+        padding-right: 20px;
+    }
 </style>
--- NUXT/5_e_commerce/e_comm_nuxt/pages/index.vue
+++ NUXT/5_e_commerce/e_comm_nuxt/pages/index.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="container">
+  <div class="mainContainer">
     <TheSlider/>
+    <!-- ürün vitrini koyacağız -->
+    <div class="productsList">
+      <div v-for="i in 12" :key="i" class="productArea">
+      product
+    </div>
+    </div>
   </div>
 </template>
 
@@ -14,5 +20,28 @@ export default {
 </script>
 
 <style>
-
+  .mainContainer{
+    display : flex;
+    flex-direction: column;
+    width: 100%;
+    align-items: center;
+  }
+  .productsList{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap; 
+    /* fazla eleman yana geldiğinde fazlayı alta at demek */
+    justify-content: space-between;
+    /* başta ve sonda boşluk bırakma */
+    width: 80%;
+    background-color: turquoise;
+    padding-right: 20px;
+    padding-left: 20px;
+  }
+  .productArea{
+    background-color: tomato;
+    height: 300px;
+    width: 300px;
+    margin-bottom: 20px;
+  }
 </style>
