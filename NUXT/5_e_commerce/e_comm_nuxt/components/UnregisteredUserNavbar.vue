508d197205ebb996b3439a05e0323ad0461b637e
kebele <ykemalbeler@gmail.com>
2020-10-17 12:38:30 +0300

    update

--- NUXT/5_e_commerce/e_comm_nuxt/components/UnregisteredUserNavbar.vue
+++ NUXT/5_e_commerce/e_comm_nuxt/components/UnregisteredUserNavbar.vue
@@ -3,13 +3,22 @@
     <b-navbar toggleable="sm" type="light" variant="light">
       <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
 
-      <b-navbar-brand>BootstrapVue</b-navbar-brand>
+      <b-navbar-brand>kebele</b-navbar-brand>
 
-      <b-collapse id="nav-text-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-text>Navbar text</b-nav-text>
-        </b-navbar-nav>
-      </b-collapse>
+      <b-nav-form>
+        <b-form-input
+          size="sm"
+          class="mr-sm-2"
+          placeholder="Search"
+        ></b-form-input>
+        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+      </b-nav-form>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item>sign in</b-nav-item>
+        <b-nav-item>sign up</b-nav-item>
+      </b-navbar-nav>
     </b-navbar>
   </div>
 </template>
