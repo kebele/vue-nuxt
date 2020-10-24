3cdea876057e7d496ffa1f5e57e3392ff6089614
kebele <ykemalbeler@gmail.com>
2020-10-24 17:47:04 +0300

    ekleme

---
+++ NUXT/7_admin/admin/components/TheNavbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-nav>
+        <b-nav-item href="#">Home</b-nav-item>
+
+        <!-- Navbar dropdowns -->
+        <b-nav-item-dropdown text="Product" right>
+          <b-dropdown-item href="/product/create">create</b-dropdown-item>
+          <b-dropdown-item href="#">delete</b-dropdown-item>
+          <b-dropdown-item href="#">update</b-dropdown-item>
+          <b-dropdown-item href="#">all</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
