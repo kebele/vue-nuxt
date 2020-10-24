a3f46951127ca4e2241677c6b90a20100a9fdb4b
kebele <ykemalbeler@gmail.com>
2020-10-24 17:30:28 +0300

    ekleme

--- NUXT/6_server/models/product.js
+++ NUXT/6_server/models/product.js
@@ -3,6 +3,11 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema;
 
+const Category = new Schema({
+    main : String,
+    sub : String
+})
+
 const ProductSchema = new Schema({
     title : String,
     price : Number,
@@ -12,10 +17,7 @@ const ProductSchema = new Schema({
     category : [Category]
 })
 
-const Category = new Schema({
-    main : String,
-    sub : String
-})
+
 
 module.exports = mongoose.model("Product", ProductSchema);
 
